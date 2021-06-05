354eb5b2d3bd0fbfeb95912029ebf5d17c357244
Angelos Ioannou <angelos.ioannou@gmx.de>
2021-06-05 02:19:50 +0200

    gyros menu category

--- src/main.js
+++ src/main.js
@@ -6,4 +6,28 @@ window.onload = function () {
     cover: true,
     lazyLoad: "nearby",
   }).mount();
+
+  menuSwitcher();
+};
+
+const menuSwitcher = () => {
+  const categories = document.querySelectorAll(".menu-cart__category");
+  const menuToggler = document.querySelectorAll(".menu-section__toggler span");
+
+  menuToggler.forEach(toggler => {
+    toggler.addEventListener("click", e => {
+      toggleActiveCategory(toggler.dataset.toggleId);
+    });
+  });
+
+  const toggleActiveCategory = id => {
+    categories.forEach(cat => {
+      if (cat.id == id) {
+        cat.classList.add("active");
+        cat.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+      } else {
+        cat.classList.remove("active");
+      }
+    });
+  };
 };
